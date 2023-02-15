7bf89061f2c8685d21642c626242f59547bfac7f
jean <christophewizamo@gmail.com>
2023-02-15 20:29:34 +0100

    midelware acces

--- back-end-lumyas/resources/views/layouts/menu.blade.php
+++ back-end-lumyas/resources/views/layouts/menu.blade.php
@@ -1,6 +1,14 @@
 <ul class="navbar-nav me-auto">
-    <li class="mx-4"><a href="{{ route('users.index') }}" style="text-decoration: none;">Users</a></li>
-    <li class="mx-4"><a href="{{ route('roles.index') }}" style="text-decoration: none;">Roles</a></li>
-    <li class="mx-4"><a href="#" style="text-decoration: none;">Categori</a></li>
-    <li class="mx-4"><a href="#" style="text-decoration: none;">Product</a></li>
+    @permission('User', 'read')
+        <li class="mx-4"><a href="{{ route('users.index') }}" style="text-decoration: none;">Users</a></li>
+    @endpermission
+    @permission('Role', 'read')
+        <li class="mx-4"><a href="{{ route('roles.index') }}" style="text-decoration: none;">Roles</a></li>
+    @endpermission
+    @permission('Role', 'read')
+        <li class="mx-4"><a href="#" style="text-decoration: none;">Categori</a></li>
+    @endpermission
+    @permission('Role', 'read')
+        <li class="mx-4"><a href="#" style="text-decoration: none;">Product</a></li>
+    @endpermission
 </ul>
