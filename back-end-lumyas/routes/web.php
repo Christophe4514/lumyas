d1477806980b785d7606e537d1f479edf8876cec
jean <christophewizamo@gmail.com>
2023-02-15 19:22:35 +0100

    midelware acces

--- back-end-lumyas/routes/web.php
+++ back-end-lumyas/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('roles', App\Http\Controllers\RoleController::class)->middleware('access:Role');
+    Route::resource('users', App\Http\Controllers\UserController::class)->middleware('access:User');
+});
