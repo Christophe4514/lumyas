f31c9b08383d50f44f4aae2d023278d04d3ecb9f
jean <christophewizamo@gmail.com>
2023-02-15 19:39:48 +0100

    menu

--- back-end-lumyas/resources/views/layouts/menu.blade.php
+++ back-end-lumyas/resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <ul class="navbar-nav me-auto">
     <li class="mx-4"><a href="{{ route('users.index') }}" style="text-decoration: none;">Users</a></li>
-    <li class="mx-4"><a href="{{ route('roles.index') }}" style="text-decoration: none;">Users</a></li>
+    <li class="mx-4"><a href="{{ route('roles.index') }}" style="text-decoration: none;">Roles</a></li>
     <li class="mx-4"><a href="#" style="text-decoration: none;">Categori</a></li>
     <li class="mx-4"><a href="#" style="text-decoration: none;">Product</a></li>
 </ul>
